203d02d61e40a79c1a5c12ace530a7b558b3ed22
Haritha Tennakoon <HarithaT@99x.lk>
2020-12-20 13:22:33 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("starting...");
+			Thread.Sleep(2000);
+			Console.WriteLine("ending...");
+			Thread.Sleep(1000);
+		}
+	}
+}
